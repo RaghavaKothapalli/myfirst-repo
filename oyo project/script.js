cfcf7cbd8ed9523cf15d1e4d5c793e77f7b527a0
Raghava Sri Vastav <kothapalliraghava2002@gmail.com>
2024-10-31 19:36:45 +0530

    changed

--- oyo project/script.js
+++ oyo project/script.js
@@ -5,10 +5,9 @@ searchBtn.addEventListener('click', () => {
   const checkIn = document.getElementById('check In').value;
   const checkOut = document.getElementById('check Out').value;
 
-  // Make API call to fetch hotel data based on the user's input
+  
   fetchHotelData(Location, checkIn, checkOut, guests)
     .then((data) => {
-      // Display the search results on the page
       renderSearchResults(data);
     })
     .catch((error) => {
@@ -17,10 +16,7 @@ searchBtn.addEventListener('click', () => {
 });
 
 function fetchHotelData(Location, checkIn, checkOut) {
-  // Implement the API call logic here
-  // Return the fetched hotel data as a Promise
 }
 
 function renderSearchResults(data) {
-  // Implement the logic to display the search results on the page
 }
